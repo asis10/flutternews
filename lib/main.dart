9147d474d94e0d92ebf2f566f7425b1786df6ff2
asis10 <flighting.nightingale@gmail.com>
2022-05-01 16:25:17 +0545

    Title Updated

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter News Demo',
+      title: 'Flutter News',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.white,
